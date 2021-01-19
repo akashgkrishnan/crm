27ec1db8c57b5c8f578e110af8b29396400d96ce
Akash <krishnanag1996@gmail.com>
2021-01-20 01:49:20 +0530

    added modal forms

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('leads/', include('leads.urls'))
+    path('leads/', include('leads.urls', namespace="leads"))
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
-from .forms import LeadForm
+from .models import Lead, Agent
+from .forms import LeadForm, LeadModelForm
 
 
 def lead_list(request):
@@ -21,17 +21,34 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == 'POST':
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            print("form is valid")
-            print(form.cleaned_data)
-            first_name = form.cleaned_data["first_name"]
-            last_name = form.cleaned_data["last_name"]
-            age = form.cleaned_data['age']
-            print(age, first_name, last_name)
+            form.save()
+            return redirect("/leads")
     context = {
         'form': form
     }
     return render(request, 'leads/lead_create.html', context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": form,
+        "lead": lead
+    }
+    return render(request, 'leads/lead_update.html', context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
