506e4f894e247df9f2579d595155030105b59c50
Akash <krishnanag1996@gmail.com>
2021-01-25 00:20:14 +0530

    added agent create and list view. added signal

--- agents/views.py
+++ agents/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
-
+from .forms import AgentModelForm
 
 class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = 'agents/agent_list.html'
@@ -12,4 +12,14 @@ class AgentListView(LoginRequiredMixin, generic.ListView):
         return Agent.objects.all()
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
-    pass
+    template_name = 'agents/agent_create.html'
+    form_class = AgentModelForm
+
+    def get_success_url(self) -> str:
+        return reverse("agents:agent-list")
+
+    def form_valid(self, form):
+        agent = form.save(commit=False)
+        agent.organisation = self.request.user.userprofile
+        agent.save()
+        return super(AgentCreateView, self).form_valid(form)
